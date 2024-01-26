2c8b89f933f33b5db4736cf05821a5208ef2c77f
Im-Rises <quentin-morel88@hotmail.com>
2024-01-25 20:59:40 -0500

    New EventSystem test version

--- Chip8topia/Chip8Emulator/Chip8Emulator.cpp
+++ Chip8topia/Chip8Emulator/Chip8Emulator.cpp
@@ -2,19 +2,12 @@
 
 #include "../Chip8topiaInputHandler/Chip8topiaInputHandler.h"
 
-void test(const uint8 key, const bool isPressed) {
-}
-
 Chip8Emulator::Chip8Emulator() {
-    //    MethodEventVarying<Chip8Emulator, int, double> test(this, &Chip8Emulator::OnInput);
-    Chip8topiaInputHandler::getInstance().m_GameInput.subscribe(&test);
-    //    Chip8topiaInputHandler::getInstance().m_GameInput.subscribe(this, &Chip8Emulator::OnInput);
-    //    MethodEventVarying test2(this, &Chip8Emulator::OnInput);
-    Chip8topiaInputHandler::getInstance().m_GameInput.trigger(0, true);
+    Chip8topiaInputHandler::getInstance().m_GameInput.subscribe(this, &Chip8Emulator::OnInput);
 }
 
 Chip8Emulator::~Chip8Emulator() {
-    //    Chip8topiaInputHandler::getInstance().m_GameInput.unsubscribe(this, &Chip8Emulator::OnInput);
+    Chip8topiaInputHandler::getInstance().m_GameInput.unsubscribe(this, &Chip8Emulator::OnInput);
 }
 
 void Chip8Emulator::readRom(const uint8* rom, const size_t romSize) {
@@ -29,6 +22,5 @@ void Chip8Emulator::update(const float deltaTime) {
 }
 
 void Chip8Emulator::OnInput(const uint8 key, const bool isPressed) {
-    // void Chip8Emulator::OnInput() {
-    //     m_core.updateKey(key, isPressed);
+    m_core.updateKey(key, isPressed);
 }
