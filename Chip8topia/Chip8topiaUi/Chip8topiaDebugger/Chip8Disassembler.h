89feeca433cb2b16e994c8b1c88d1188c096d034
Im-Rises <quentin-morel88@hotmail.com>
2024-02-10 22:58:44 -0500

    Writing disassembly

--- Chip8topia/Chip8topiaUi/Chip8topiaDebugger/Chip8Disassembler.h
+++ Chip8topia/Chip8topiaUi/Chip8topiaDebugger/Chip8Disassembler.h
@@ -1,4 +1,25 @@
 #pragma once
 
+#include <binaryLib/binaryLib.h>
+#include <array>
+#include <string>
+#include <bitset>
+#include "../../Chip8Emulator/Chip8Core/Core/Cpu.h"
+
 class Chip8Disassembler {
+public:
+    Chip8Disassembler() = default;
+    Chip8Disassembler(const Chip8Disassembler&) = delete;
+    Chip8Disassembler(Chip8Disassembler&&) = delete;
+    auto operator=(const Chip8Disassembler&) -> Chip8Disassembler& = delete;
+    auto operator=(Chip8Disassembler&&) -> Chip8Disassembler& = delete;
+    ~Chip8Disassembler() = default;
+
+public:
+    void disasemble(const std::array<uint8, Cpu::MEMORY_SIZE>& memory, uint16 pc);
+    void draw();
+
+private:
+    std::array<std::string, Cpu::MEMORY_SIZE> m_disassembly{ "D" };
+    std::bitset<Cpu::MEMORY_SIZE> m_breakpoints;
 };
