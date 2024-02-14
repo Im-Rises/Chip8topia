340893784852596aaec5de80a7cc0faae8dccec9
Im-Rises <quentin-morel88@hotmail.com>
2024-02-14 18:27:02 -0500

    wip

--- Chip8topia/main.cpp
+++ Chip8topia/main.cpp
@@ -19,7 +19,12 @@
 //    }
 //}
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(_DEBUG)
+#define WIN32_RELEASE 1
+#endif
+
+
+#if defined(WIN32_RELEASE)
 #include <Windows.h>
 #else
 #include <iostream>
@@ -27,24 +32,21 @@
 
 #include "Chip8topia.h"
 
-#ifdef _WIN32
+#if defined(WIN32_RELEASE)
 auto WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) -> int {
     (void)hInstance;
     (void)hPrevInstance;
     (void)lpCmdLine;
     (void)nCmdShow;
-
-    Chip8topia chip8topia;
-
-    return chip8topia.run();
-}
 #else
 auto main(int argc, char* argv[]) -> int {
     (void)argc;
     (void)argv;
+#endif
 
     Chip8topia chip8topia;
 
+#ifndef WIN32_RELEASE
     std::cout << Chip8topia::PROJECT_NAME << " " << Chip8topia::PROJECT_VERSION << std::endl
               << Chip8topia::PROJECT_LINK << std::endl
               << Chip8topia::PROJECT_AUTHOR << "\n"
@@ -70,7 +72,7 @@ auto main(int argc, char* argv[]) -> int {
               << " - Glad version " << Chip8topia::getGladVersion() << std::endl
               << " - ImGui version " << Chip8topia::getImGuiVersion() << std::endl
               << std::endl;
+#endif
 
     return chip8topia.run();
 }
-#endif
