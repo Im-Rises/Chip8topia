3c54a76e9602f0394c44f28f4e9864ff9fee2f35
Im-Rises <quentin-morel88@hotmail.com>
2024-04-07 23:11:37 -0400

    Added updated version of getKeyWaitReleased

--- Chip8topia/Chip8Emulator/Chip8CoreBase/Core/Input.cpp
+++ Chip8topia/Chip8Emulator/Chip8CoreBase/Core/Input.cpp
@@ -19,36 +19,56 @@ auto Input::isKeyPressed(const uint8 Vx) const -> bool
 
 auto Input::getKeyWaitReleased() -> int
 {
-    switch (m_waitKeyStep)
-    {
-    case 0:
-    {
-        for (int i = 0; i < KEY_COUNT; i++)
-        {
-            if (m_pressedKeys[i])
-            {
-                m_keysWaitPreviousState[i] = true;
-                m_waitKeyStep = 1;
-            }
-        }
-        return -1;
-    }
-    case 1:
+    /* This version check if there is a difference and then iterate to find the key */
+    int key = -1;
+    std::bitset<KEY_COUNT> pressedKeys = m_pressedKeys;
+
+    auto keysReleased = pressedKeys.flip() & m_keysWaitPreviousState;
+    if (keysReleased.any())
     {
         for (int i = 0; i < KEY_COUNT; i++)
         {
-            if (m_keysWaitPreviousState[i] && !m_pressedKeys[i])
+            if (keysReleased[i])
             {
-                m_keysWaitPreviousState.reset();
-                m_waitKeyStep = 0;
-                return i;
+                key = i;
             }
         }
-        return -1;
     }
-    default:
-    {
-        return -1;
-    }
-    }
+
+    m_keysWaitPreviousState = m_pressedKeys;
+    return key;
+
+    /* First version check all key states to find a key which was pressed and now released, it does it every time which is not optimal */
+    //    switch (m_waitKeyStep)
+    //    {
+    //    case 0:
+    //    {
+    //        for (int i = 0; i < KEY_COUNT; i++)
+    //        {
+    //            if (m_pressedKeys[i])
+    //            {
+    //                m_keysWaitPreviousState[i] = true;
+    //                m_waitKeyStep = 1;
+    //            }
+    //        }
+    //        return -1;
+    //    }
+    //    case 1:
+    //    {
+    //        for (int i = 0; i < KEY_COUNT; i++)
+    //        {
+    //            if (m_keysWaitPreviousState[i] && !m_pressedKeys[i])
+    //            {
+    //                m_keysWaitPreviousState.reset();
+    //                m_waitKeyStep = 0;
+    //                return i;
+    //            }
+    //        }
+    //        return -1;
+    //    }
+    //    default:
+    //    {
+    //        return -1;
+    //    }
+    //    }
 }
