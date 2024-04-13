ade3ae5130ff5668f7957b8b0f14cc1b1f6c9c8f
Im-Rises <quentin-morel88@hotmail.com>
2024-04-13 14:57:54 -0400

    wip

--- Chip8topia/Chip8Emulator/Chip8Emulator/Chip8SoundEmulation.cpp
+++ Chip8topia/Chip8Emulator/Chip8Emulator/Chip8SoundEmulation.cpp
@@ -4,6 +4,25 @@
 
 #include "../Chip8CoreBase/Chip8CoreBase.h"
 
+Chip8SoundEmulation::Chip8SoundEmulation()
+{
+    //    SDL_AudioSpec spec = {
+    //        .freq = FREQUENCY_RESOLUTION,
+    //        .format = AUDIO_S16SYS, // Signed 16 bit integer format
+    //        .channels = 1,
+    //        .samples = 4096,         // The size of each "chunk"
+    //        .callback = play_buffer, // user-defined function that provides the audio data
+    //        .userdata = nullptr      // an argument to the callback function (we dont need any)
+    //    };
+    //
+    //    SDL_AudioDeviceID dev = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
+    //
+    //    //    for (int i = 0; i < BUFFER_LEN; i++)
+    //    //    {
+    //    //        buffer[i] = format(tone(440, i), 0.5);
+    //    //    }
+}
+
 void Chip8SoundEmulation::update(Chip8CoreBase* chip8Core)
 {
     //    // get ST
--- Chip8topia/Chip8Emulator/Chip8Emulator/Chip8SoundEmulation.h
+++ Chip8topia/Chip8Emulator/Chip8Emulator/Chip8SoundEmulation.h
@@ -3,6 +3,17 @@
 class Chip8CoreBase;
 class Chip8SoundEmulation
 {
+private:
+    static constexpr int FREQUENCY_RESOLUTION = 48000;
+
+public:
+    Chip8SoundEmulation();
+    Chip8SoundEmulation(const Chip8SoundEmulation&) = delete;
+    Chip8SoundEmulation(Chip8SoundEmulation&&) = delete;
+    auto operator=(const Chip8SoundEmulation&) -> Chip8SoundEmulation& = delete;
+    auto operator=(Chip8SoundEmulation&&) -> Chip8SoundEmulation& = delete;
+    ~Chip8SoundEmulation() = default;
+
 public:
     void update(Chip8CoreBase* chip8Core);
 };
