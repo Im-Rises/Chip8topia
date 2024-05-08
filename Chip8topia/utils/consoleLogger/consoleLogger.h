7644d86ce2d253b988349675a9651183a31ecc4f
Im-Rises <quentin-morel88@hotmail.com>
2024-05-08 18:37:35 -0400

    Exception handling not working...

--- Chip8topia/utils/consoleLogger/consoleLogger.h
+++ Chip8topia/utils/consoleLogger/consoleLogger.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #if defined(BUILD_RELEASE)
-#define LOG_ERROR(message)
-#define LOG_WARNING(message)
-#define LOG_INFO(message)
-#define LOG_SUCCESS(message)
+#define LOG_ERROR(message) (void)0
+#define LOG_WARNING(message) (void)0
+#define LOG_INFO(message) (void)0
+#define LOG_SUCCESS(message) (void)0
 #else
 #if defined(__EMSCRIPTEN__)
 #include <iostream>
 #define LOG_ERROR(message) std::cerr << message << '\n'
 #define LOG_WARNING(message) std::cout << message << '\n'
-#define LOG_INFO(message) std::clog << message << '\n'
+#define LOG_INFO(message) std::cout << message << '\n'
 #define LOG_SUCCESS(message) std::cout << message << '\n'
 #else
 #include <spdlog/spdlog.h>
@@ -20,3 +20,5 @@
 #define LOG_SUCCESS(message) spdlog::info(message)
 #endif
 #endif
+
+// TODO: maybe use std::clog instead of std::cout for LOG_INFO but it's not correct in emscripten it correspond to an error message
